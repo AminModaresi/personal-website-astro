926b5129a27900e3d6e1eb125729db89b5da5702
AminModaresi <aminiiiiiiiuii@gmail.com>
2025-03-20 09:30:03 +0330

    bug fixed

--- src/components/common/BorderContainer.tsx
+++ src/components/common/BorderContainer.tsx
@@ -6,6 +6,6 @@ interface BorderContainerProps {
 }
 
 const BorderContainer = ({ children , className = "" }: BorderContainerProps) => {
-  return <section className={`py-12 px-4 border-b border-dashed ${className}`}>{children}</section>;
+  return <section className={`py-12 px-4 border-b border-dashed border-neutral-400 dark:border-neutral-800 ${className}`}>{children}</section>;
 };
 export default BorderContainer
