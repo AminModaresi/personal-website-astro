e82d4ff5163fb2745ef89b5b85b26ef3892ef3aa
AminModaresi <aminiiiiiiiuii@gmail.com>
2025-08-03 05:35:25 -0700

    fix: change output configuration to 'static' and restore experimental features

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,13 +9,16 @@ import cloudflare from '@astrojs/cloudflare';
 // https://astro.build/config
 export default defineConfig({
   // Since you're using static site generation
-  output: 'server',
+  output: 'static',
 
   vite: {
     plugins: [tailwindcss()]
   },
-  experimental: {
-    session: true
+ experimental: {
+    responsiveImages: true,      // optional in v5.4
+    clientPrerender: true,       // if you use `prefetch: true`
+    contentIntellisense: true,   // if using `.mdx/.mdoc` content collections
+    session : true,          // if you use `session` in your components
   },
 
   integrations: [react(), icon()],
